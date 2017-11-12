750e3f3b01f6c029b70b553ff0432f5bb8bb1f72
Angelo Cigna <acigna1982@gmail.com>
2017-11-12 18:06:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
 // Code your solution in this file
 function findMatching(drivers, name) {
   return drivers.filter(function(driver){
-    if(name === driver) {
+    if(name.tolowercase() === driver) {
       return true
-
     }
     else {
       return false
     }
-  }
+  })
 
-)
 
+return matchingDrivers;
 }
