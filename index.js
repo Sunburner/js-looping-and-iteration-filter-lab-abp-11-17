8006fe01278923d108752731f0c9b92b848a67b7
Angelo Cigna <acigna1982@gmail.com>
2017-11-12 18:07:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 // Code your solution in this file
 function findMatching(drivers, name) {
   let matchingDrivers = drivers.filet(function(driver)) {
-  return drivers.filter(function(driver){
-    if(name.tolowercase() === driver) {
-      return true
-    }
-    else {
-      return false
-    }
-  })
+    return drivers.filter(function(driver){
+      if(name.tolowercase() === driver) {
+        return true
+      }
+      else {
+        return false
+      }
+    })
 
 
 return matchingDrivers;
