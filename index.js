d4604e7bfde23b7b99c1cc863045d12ccc080f7e
Angelo Cigna <acigna1982@gmail.com>
2017-11-12 18:05:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 // Code your solution in this file
-function findMatching() {
+function findMatching(drivers, name) {
+  drivers.filter(function(driver){
+    if(name === driver) {
+      return true
+      
+    }
+    else {
+      return false
+    }
+  }
 
-  
+)
+
 }
